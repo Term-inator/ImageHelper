3366da73d97e413736241d6bf55f5872cbdcaf2a
Term-inator <1121745627@qq.com>
2023-12-24 17:44:24 -0600

    修改单个文件的修改时间

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 运行 dhash.py 进行图片去重，出现相似图片时，输入 index 选择一张删除，输入 n 跳过
 
-运行 rename.py 进行图片重命名
+运行 rename.py 根据修改时间重命名图片
 
 folders.yaml 为文件夹配置文件，格式如下：
 
@@ -19,3 +19,5 @@ folder:
                 - name: xxx
         - name: xxx
 ```
+
+utime.py 为修改文件夹和文件修改时间的脚本
--- utime.py
+++ utime.py
@@ -1,21 +1,41 @@
-"""
-修改指定文件夹下直接文件的修改时间为指定时间
-"""
-
 import os
 import datetime
 
-# 替换以下路径为你要遍历的文件夹路径
-folder_path = "D:/csc/Pictures/2023-04-20"
 
-# 用指定的格式将字符串转换为 datetime 对象
-date_string = "2019-10-14 18:58:00"
 format_string = "%Y-%m-%d %H:%M:%S"
 
-specified_datetime = datetime.datetime.strptime(date_string, format_string)
 
-for file in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, file)
-    if os.path.isfile(file_path):
-        os.utime(file_path, (specified_datetime.timestamp(), specified_datetime.timestamp()))
-        print(f"Modified {file_path} modification time.")
+def set_file_time(file_path, date_string):
+    """
+    修改指定文件的修改时间为指定时间
+    """
+    specified_datetime = datetime.datetime.strptime(date_string, format_string)
+
+    # 修改文件的访问时间和修改时间
+    os.utime(file_path, (specified_datetime.timestamp(), specified_datetime.timestamp()))
+    print(f"Modified {file_path} modification time.")
+
+
+def set_folder_time(folder_path, date_string):
+    """
+    修改指定文件夹下直接文件的修改时间为指定时间
+    """
+    specified_datetime = datetime.datetime.strptime(date_string, format_string)
+
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path):
+            # 修改文件的访问时间和修改时间
+            os.utime(file_path, (specified_datetime.timestamp(), specified_datetime.timestamp()))
+            print(f"Modified {file_path} modification time.")
+
+
+if __name__ == '__main__':
+    # 替换以下路径为你要遍历的文件夹路径
+    # folder_path = "D:/csc/Pictures/2023-04-20"
+    file_path = r"D:\csc\Pictures\All\2023-12-23\2023\12\20231222_192203_Fri_99934.jpg"
+
+    # 用指定的格式将字符串转换为 datetime 对象
+    date_string = "2023-09-09 19:48:00"
+
+    set_file_time(file_path, date_string)
